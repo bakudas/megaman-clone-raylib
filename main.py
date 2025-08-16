3e25e2a2d08f1b1a1f6376037fb2760a6c8d1770
bakudas <bakudas@gmail.com>
2025-08-16 18:37:36 -0300

    feat(camera): implement smooth follow-camera with look ahead

--- main.py
+++ main.py
@@ -10,29 +10,38 @@ from game.platforms import Platform
 
 # 1. Inicialização
 # -------------------------------------------------
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 256
+SCREEN_HEIGHT = 224
 FLOOR_LEVEL = 400
 
 pr.init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Mega Man TDD Curso")
 pr.set_target_fps(60)
 
+# Inicializar a Camera
+camera = pr.Camera2D()
+camera.target = pr.Vector2(0, 0)  # o ponto que a camera olha
+camera.offset = pr.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # centro da tela
+camera.rotation = 0.0
+camera.zoom = 0.5
+
 # Cria uma lista de plataformas para definir o nível
 level_platforms = [
     # Chão
-    Platform(0, 400, SCREEN_WIDTH, 50, "solid"),
+    Platform(0, 224, SCREEN_WIDTH, 16, "solid"),
     # Plataformas no ar
-    Platform(100, 300, 150, 20, "solid"),
-    Platform(300, 250, 100, 20, "pass-through"),
-    Platform(500, 200, 200, 20, "solid"),
+    Platform(96, 96, 64, 16, "solid"),
+    Platform(128, 128, 96, 16, "pass-through"),
+    Platform(156, 156, 64, 16, "solid"),
 ]
 
 # Estado inicial do jogador
-player = Player(x=SCREEN_WIDTH / 2, y=0, width=40, height=50, speed=5, jump_strength=12)
+player = Player(
+    x=SCREEN_WIDTH / 2, y=0, width=8, height=16, speed=2.5, jump_strength=7.5
+)
 
 # Configuração da física do nosso mundo
 world_physics = {
-    "gravity": 0.5,  # um valor menor funciona melhor para 60 FPS
+    "gravity": 0.75,  # um valor menor funciona melhor para 60 FPS
     "platforms": level_platforms,
 }
 
@@ -75,18 +84,29 @@ def run_game():
         # List comprehension para criar uma nova lista apenas com as balas visíveis
         bullets = [b for b in bullets if 0 < b.x_pos < SCREEN_WIDTH]
 
+        # Atualizar o alvo da camera
+        # o alvo inicial é um pocuo a frente do jogador
+        look_ahead = 5 * (1 if player.facing_direction == "RIGHT" else -1)
+        target_x = player.x_pos + (player.width / 2) * look_ahead
+        target_y = player.y_pos + (player.height / 2) - 100  # olhar um pocuo para cima
+
+        # Suavizar o movimento da camera usando interpolação linear 'lerp'
+        # a camera se move 5% da distância até o alvo a cada frame
+        smoothing_factor = 0.025
+        camera.target.x += (target_x - camera.target.x) * smoothing_factor
+        camera.target.y += (target_y - camera.target.y) * smoothing_factor
+
         # 4. Draw
         pr.begin_drawing()
-
         pr.clear_background(pr.DARKGRAY)
+
+        # inicia o modo de camera 2D
+        pr.begin_mode_2d(camera)
 
         # Desenha as plataformas
         for plat in world_physics["platforms"]:
             color = pr.GRAY if plat.type == "solid" else pr.LIGHTGRAY
             pr.draw_rectangle(plat.x, plat.y, plat.width, plat.height, color)
-
-        # Texto debug
-        pr.draw_text("Nosso 'Mega Man' caindo!", 200, 20, 20, pr.LIGHTGRAY)
 
         # Desenha o jogador
         # Usamos os dados do nosso dic 'player_state'
@@ -107,6 +127,28 @@ def run_game():
                 bullet.height,
                 pr.YELLOW,
             )
+
+        pr.draw_text(
+            f"""
+            player starts:
+            x: {player.x_pos}
+            y: {player.y_pos}
+            x_vel: {player.x_vel}
+            y_vel: {player.y_vel}
+            is_grounded: {player.is_on_ground(world_physics)}
+            """,
+            50,
+            50,
+            20,
+            pr.LIGHTGRAY,
+        )
+
+        # termina o modo de camera 2D
+        pr.end_mode_2d()
+
+        # UI
+        # Texto debug
+        pr.draw_text("Just another Megaman clone...", 50, 50, 24, pr.LIGHTGRAY)
 
         pr.end_drawing()
 
